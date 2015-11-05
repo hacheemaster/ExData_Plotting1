aac4962dc24ec6c64a602fa1a498420d95cefa35
ike <hacheemaster@gmail.com>
2015-11-05 11:49:08 -0800

    updated plot4 to not shrink legend

--- plot4.R
+++ plot4.R
@@ -29,7 +29,7 @@ plot(date_and_time,data_subset$Sub_metering_1,
 lines(date_and_time,data_subset$Sub_metering_2,type="l",col="red")
 lines(date_and_time,data_subset$Sub_metering_3,type="l",col="blue")
 legend("topright", lty=1,legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
-       col=c("black","blue","red"), cex=0.6)
+       col=c("black","blue","red"))
 
 #plot 3
 plot(date_and_time,Voltage,
